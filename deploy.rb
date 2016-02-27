dd6c8b941deb0e623997687b6e14e97c3711f0c2
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-02-26 18:22:45 -0800

    Precompiles assets

---
+++ deploy.rb
@@ -0,0 +1,21 @@
+def precompile_assets?
+  loop do
+    print 'Precompile assets? (Y/N)  '
+    answer = gets.chomp
+    return true  if answer.downcase == 'y'
+    return false if answer.downcase == 'n'
+    puts ' > Please respond with Y or N'
+  end
+end
+
+if precompile_assets?
+  puts 'Precompiling assets...'
+  `RAILS_ENV=production bundle exec rake assets:precompile`
+  puts 'Committing...'
+  puts `git add .`
+  puts `git commit -m 'Precompiles assets'`
+end
+
+puts `git push`
+puts `/usr/local/heroku/bin/heroku run rake db:migrate --app nosko-staging`
+exec '/usr/local/heroku/bin/heroku logs -t --app nosko-staging'
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-RAILS_ENV=production bundle exec rake assets:precompile
-git add .
-git commit -m 'Precompiles assets'
-git push
-heroku run rake db:migrate --app nosko-staging
