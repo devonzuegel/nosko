849570f22d95d4966eefcaf83ca3f4417a5de7c8
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-02-26 18:42:37 -0800

    Update deploy_production.rb
    Minor change

--- deploy_production.rb
+++ deploy_production.rb
@@ -1,14 +1,18 @@
-loop do
-  print 'About to promote `nosko-staging` (staging) to `nosko` (production). Continue?  '
-  answer = gets.chomp
-  return true  if answer.downcase == 'y'
-  return false if answer.downcase == 'n'
-  puts '  > Please respond with Y or N'
+def promote_to_production?
+  loop do
+    print 'About to promote `nosko-staging` (staging) to `nosko` (production). Continue?  '
+    answer = gets.chomp
+    return true  if answer.downcase == 'y'
+    return false if answer.downcase == 'n'
+    puts '  > Please respond with Y or N'
+  end
 end
 
-puts 'Promoting...'
-puts `/usr/local/heroku/bin/heroku pipelines:promote --app nosko-staging`
-puts 'Migrating...'
-puts `/usr/local/heroku/bin/heroku run rake db:migrate --app nosko`
-puts 'Opening logs...'
-exec '/usr/local/heroku/bin/heroku logs -t --app nosko'
+if promote_to_production?
+  puts 'Promoting...'
+  puts `/usr/local/heroku/bin/heroku pipelines:promote --app nosko-staging`
+  puts 'Migrating...'
+  puts `/usr/local/heroku/bin/heroku run rake db:migrate --app nosko`
+  puts 'Opening logs...'
+  exec '/usr/local/heroku/bin/heroku logs -t --app nosko'
+end
