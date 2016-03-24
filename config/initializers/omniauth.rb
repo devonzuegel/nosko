b91c292d3766fb8ccd2d44ed78856f1c463709c8
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-03-24 01:03:07 -0700

    Adds evernote oauth info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['omniauth_provider_key'], ENV['omniauth_provider_secret']
+  provider :twitter,  ENV['omniauth_provider_key'], ENV['omniauth_provider_secret']
+  provider :evernote, ENV['en_consumer_key'],       ENV['en_secret_key'],   client_options: { site: ENV['en_oauth_site'] }
 end
