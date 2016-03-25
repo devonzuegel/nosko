154f64d4fcea81243b9f32df2bb78e02dd9fe04b
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-03-24 22:17:47 -0700

    Add optional evernote_note attribute to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < Finding
+  has_one   :evernote_note, dependent: :destroy
   validates :content, presence: true, blank: false
 
   REQUIRED_FIELDS = superclass::REQUIRED_FIELDS + %i(content)
