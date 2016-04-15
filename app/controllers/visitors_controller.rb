e7e333b50c6e9820500f4b9b319e782c7104bdb3
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-14 22:46:12 -0700

    Implement basic user profile

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,12 +2,10 @@ class VisitorsController < ApplicationController
   def index
     # if user_signed_in?
     #   if Finding::Article.count == 0
-    #     en_account = current_user.evernote_account
-    #     en_account.sync_notes if current_user.evernote_connected?
+    #     current_user.evernote_account.sync if current_user.evernote_connected?
     #   end
     #   @articles = Finding::Article.where(user: current_user)
-    # else
-    @articles = Finding::Article.limit(10).map { |a| a.title }
     # end
+    @articles = Finding::Article.limit(10)
   end
 end
--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -1,3 +1,3 @@
 Que.wake_interval = (ENV['QUE_INTERVAL'] || 15).to_i.seconds
-Que.mode          = :off #(ENV['QUE_MODE']     || :async).to_sym
+Que.mode          = (ENV['QUE_MODE']     || :async).to_sym
 Que.worker_count  = (ENV['QUE_WORKERS']  || 1).to_i
