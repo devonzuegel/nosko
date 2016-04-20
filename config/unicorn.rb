58912da087b9951e287c45dd9d758b32b629b032
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-20 14:15:11 -0700

    Fixes Que initialization

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,11 @@
-# after_fork do |server, worker|
-#   ActiveRecord::Base.establish_connection
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
-#   Que.wake_interval = (ENV['QUE_WAKE_INTERVAL'] || 10).to_i.seconds
-#   Que.mode          = (ENV['QUE_MODE'] || :async).to_sym
-#   Que.worker_count  = (ENV['QUE_WORKERS'] || 1)
-# end
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+
+  Que.wake_interval = (ENV['QUE_WAKE_INTERVAL'] || 10).to_i.seconds
+  Que.mode          = (ENV['QUE_MODE'] || :async).to_sym
+  Que.worker_count  = (ENV['QUE_WORKERS'] || 1)
+end
