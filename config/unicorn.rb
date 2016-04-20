9310d80cdd34f80f389e6e7824965aff9c5af1d8
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-20 14:17:42 -0700

    Fixes Que initialization

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,5 +7,5 @@ after_fork do |server, worker|
 
   Que.wake_interval = (ENV['QUE_WAKE_INTERVAL'] || 10).to_i.seconds
   Que.mode          = (ENV['QUE_MODE'] || :async).to_sym
-  Que.worker_count  = (ENV['QUE_WORKERS'] || 1)
+  Que.worker_count  = (ENV['QUE_WORKERS'] || 1).to_i
 end
