4374f548bf07a6041eca3544383bddad54cac524
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-08 01:51:24 -0700

    Update navigation layout with dropdown
    Update navigation layout with dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,8 @@
 module ApplicationHelper
-  def link_fa_to(icon_name, link, text = nil)
-    link_to content_tag(:i, text, :class => "fa fa-#{icon_name}"), link
-  end
-
   def icon_link(icon_name, link, text = nil, classes: [])
-    innertext      = text.nil? ? nil : content_tag(:div, text, :class => 'icon-text')
-    contextclasses = "ion-icon ion-#{icon_name} #{classes.join(' ')}"
     link_to link do
-      content_tag :div do
-        content_tag(:div, nil, :class => contextclasses) + content_tag(:div, text, :class => 'icon-text')
+      content_tag :div, :class => classes.join(' ') do
+        content_tag(:div, nil, :class => "ion-icon ion-#{icon_name}") + content_tag(:div, text, :class => 'icon-text')
       end
     end
   end
