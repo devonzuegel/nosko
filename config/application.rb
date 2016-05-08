56688a337dc08264a48746d60e42ef97e698b0c9
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-08 00:50:11 -0700

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Nosko
   class Application < Rails::Application
     config.assets.paths << Rails.root.join(*%w(app assets stylesheets partials))
+    config.assets.paths << Rails.root.join(*%w(app assets stylesheets fonts))
 
     config.autoload_paths += %W(
       #{config.root}/app/controllers/concerns/**/
