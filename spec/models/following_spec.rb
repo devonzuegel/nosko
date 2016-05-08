7b2e7510dd7b64f1cf2611e4e0e1160af18d5cbc
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-08 02:27:29 -0700

    Add Following specs

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
-  it 'should not allow a user to double-follow another user'
-  it 'should require both leader and follower to be defined'
+  before(:all) do
+    @user       = create(:user)
+    @other_user = create(:user)
+  end
+
+  it 'should not allow a user to double-follow another user' do
+    @user.follow!(@other_user)
+    expect(@user.follow!(@other_user)).to eq false
+    expect(@user.errors.full_messages).to eq ["You're already following user ##{@other_user.id}"]
+  end
+
+  it 'should require both leader and follower to be defined' do
+    expect { @user.follow!(nil) }.to raise_error ActiveRecord::RecordInvalid
+  end
 end
