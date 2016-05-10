96a4deb8112b12095c42fbfa941e8100537df8ac
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-10 15:35:11 -0700

    Add .card-buttons
    Add .card-buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
-  def icon_link(icon_name, link, text = nil, classes: [])
+  def icon_link(icon_name, link, text = nil, classes: [], id: nil)
     icon = content_tag(:div, nil, :class => "ion-icon ion-#{icon_name}")
     text = content_tag(:div, text, :class => 'icon-text')
 
     link_to link do
-      content_tag :div, :class => classes.join(' ') { icon + text }
+      content_tag :div, :class => classes.join(' '), id: id do
+        icon + text
+      end
     end
   end
 end
