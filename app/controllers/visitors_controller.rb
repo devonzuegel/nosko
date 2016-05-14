f82cd7bceb0324cb642ef2f931b2d741a29b9f1c
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-14 16:08:22 -0700

    Can only see articles of anyone if signed in (temporary privacy hack)

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,15 @@
 class VisitorsController < ApplicationController
   def index
-    # if user_signed_in?
     #   if Finding::Article.count == 0
     #     current_user.evernote_account.sync if current_user.evernote_connected?
     #   end
     #   @articles = Finding::Article.where(user: current_user)
     # end
 
-    @articles = Finding::Article.first(20).map { |a| a.decorate.as_prop }
+    if user_signed_in?
+      @articles = Finding::Article.first(20).map { |a| a.decorate.as_prop }
+    else
+      @articles = []
+    end
   end
 end
