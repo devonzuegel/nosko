0e0c8b4dcceb62c20076169c014a5beb7346519c
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-16 19:41:07 -0700

    Update sharing_spec

--- spec/models/sharing_spec.rb
+++ spec/models/sharing_spec.rb
@@ -11,24 +11,24 @@ RSpec.describe Sharing, type: :model do
   end
 
   it 'should allow you to update :share_by_default' do
-    Sharing.share_by_default_options.reverse_each do |opt|
-      expect { sharing.update_attributes(share_by_default: opt) }.to change { sharing.share_by_default }.to(opt)
+    Sharing.share_by_defaults.keys.reverse_each do |opt|
+      expect { sharing.update_attributes(share_by_default: opt) }
+                          .to change { sharing.share_by_default }.to(opt)
     end
   end
 
   it 'should allow you to update :reminders_frequency' do
-    Sharing.reminders_frequency_options.reverse_each do |freq|
-      expect { sharing.update_attributes(reminders_frequency: freq) }.to change { sharing.reminders_frequency }.to(freq)
+    Sharing.reminders_frequencies.keys.reverse_each do |freq|
+      expect { sharing.update_attributes(reminders_frequency: freq) }
+                          .to change { sharing.reminders_frequency }.to(freq)
     end
   end
 
   it 'should have the expected share_by_default_options' do
     expect(Sharing.share_by_defaults).to eq({ 'Only me' => 0, 'Friends' => 1, 'Public' => 2 })
-    expect(Sharing.share_by_default_options).to eq ['Only me', 'Friends', 'Public']
   end
 
   it 'should have the expected reminders_frequency_options' do
     expect(Sharing.reminders_frequencies).to eq({ 'Daily' => 0, 'Every two days' => 1, 'Weekly' => 2 })
-    expect(Sharing.reminders_frequency_options).to eq ['Daily', 'Every two days', 'Weekly']
   end
 end
