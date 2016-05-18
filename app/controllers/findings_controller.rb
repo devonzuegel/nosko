92064e4b91669563b4555b4925245242aad51868
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-18 00:54:58 -0700

    Refactor :find_permalink before_action for Findings controller

--- app/controllers/findings_controller.rb
+++ app/controllers/findings_controller.rb
@@ -1,42 +1,35 @@
 class FindingsController < ApplicationController
-  def show
-    permalink = Permalink.find_by_path(params[:permalink])
+  before_action :find_permalink
 
-    if permalink.nil?
-      respond_to do |format|
-        format.html { render_404 }
-        format.json { head :not_found }
-      end
-    else
-      @article = Finding::Article.find_by(permalink: permalink).decorate
-      respond_to do |format|
-        format.html
-        format.json { render json: @article.as_prop, status: :ok }
-      end
+  def show
+    @article = Finding::Article.find_by(permalink: @permalink).decorate
+    respond_to do |format|
+      format.html
+      format.json { render json: @article.as_prop, status: :ok }
     end
   end
 
   def lock
-    permalink = Permalink.find_by_path(params[:permalink])
-
-    if permalink.nil?
-      head :not_found
-    else
-      a = Finding::Article.find_by(permalink: permalink)
-      a.lock!
-      head :ok
-    end
+    a = Finding::Article.find_by(permalink: @permalink)
+    a.lock!
+    head :ok
   end
 
   def unlock
-    permalink = Permalink.find_by_path(params[:permalink])
+    a = Finding::Article.find_by(permalink: @permalink)
+    a.unlock!
+    head :ok
+  end
 
-    if permalink.nil?
-      head :not_found
-    else
-      a = Finding::Article.find_by(permalink: permalink)
-      a.unlock!
-      head :ok
+  private
+
+  def find_permalink
+    @permalink = Permalink.find_by_path(params[:permalink])
+    return unless @permalink.nil?
+
+    respond_to do |format|
+      format.html { render_404 }
+      format.json { head :not_found }
     end
   end
 end
