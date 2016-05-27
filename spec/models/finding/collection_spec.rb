5af0bf3a99f49392619594c54dc2e5408a05d67a
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-27 16:50:50 -0700

    Add specs for finding types within collection
    Add specs for finding types within collection
    
    Add specs for finding types within collection

--- spec/models/finding/collection_spec.rb
+++ spec/models/finding/collection_spec.rb
@@ -19,12 +19,20 @@ RSpec.describe Finding::Collection, type: :model do
       @collection = Finding::Collection.new(@user)
       traits = Shareable::SHARE_BY_DEFAULT_ENUM.keys.map { |k| k.downcase.tr(' ', '_').to_sym }
       traits.each { |t| create(:article, t, user: @user) }
+
+      create(:article)  # An article that doesn't belong to @user
     end
 
-    it 'does something' do
+    it 'retrieve all of the user\'s articles' do
       expect(@collection.all).to match @user.articles
       expect(@collection.all.length).to eq 3
     end
+  end
+
+  # The following specs will expand over time as we add more finding types
+
+  it 'should have the expected finding types listed' do
+    expect(Finding::Collection::TYPES).to eq({ article: Finding::Article })
   end
 
   it 'should have the expected sibling constants' do
