38a763d8ac3a191312e57d106aa9cf2f6b3c2bb1
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-06-02 17:16:24 -0700

    Add information about localhost tunnel for Messenger bot testing to readme

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,7 +1,6 @@
 class MessengerBotController < ActionController::Base
   def message(event, sender)
-    profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
-    ap profile
+    # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     sender.reply({ text: "Reply: #{event['message']['text']}" })
   end
 
